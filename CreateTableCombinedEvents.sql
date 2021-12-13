f899b317a42469cf70a65259eb63878bf4006143
lukedesando <Lukedesando@gmail.com>
2021-12-13 16:11:43 -0500

    Added Actual BA, Change Order Functions

--- CreateTableCombinedEvents.sql
+++ CreateTableCombinedEvents.sql
@@ -1,13 +1,14 @@
 Create TABLE CombinedEvents(
+    pitcher_name tinytext,
     pitcher int,
-    pitcher_name text,
     batter int,
-    batter_name text,
-    estimated_ba DOUBLE,
+    batter_name tinytext,
+    PA smallINT,
+    AB smallINT,
+    Hits smallINT,
     actual_ba DOUBLE,
-    PA INT,
-    AB INT,
-    Hits INT
+    estimated_ba DOUBLE,
+    estimated_minus_actual_ba DOUBLE
 )
 select
     pitcher,
@@ -15,7 +16,7 @@ select
     player_name as batter_name,
     avg(estimated_ba_using_speedangle) as estimated_ba,
     count(batter) as PA,
-    sum(babip_value) as Hits
+    SUM(babip_value)+count(CASE events when 'home_run' then 1 else null end) as Hits,
 from
     DatabaseBatterEvents
 group by
@@ -30,12 +31,26 @@ group by batter, pitcher) b
 	on c.batter = b.batter and c.pitcher = b.pitcher
 Set c.AB = b.AB;
 
+update CombinedEvents set actual_ba = Hits/AB;
+
+update CombinedEvents
+set estimated_minus_actual_ba = estimated_ba-actual_ba;
+
+ALTER TABLE `GameLogs`.`CombinedEvents`
+CHANGE `pitcher` `pitcher` int(11) NULL DEFAULT NULL COMMENT '' AFTER `pitcher_name`,
+CHANGE `batter` `batter` int(11) NULL DEFAULT NULL COMMENT '' AFTER `pitcher`,
+CHANGE `batter_name` `batter_name` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '' AFTER `batter`,
+CHANGE `PA` `PA` tinyint NULL DEFAULT NULL COMMENT '' AFTER `batter_name`,
+CHANGE `AB` `AB` tinyint NULL DEFAULT NULL COMMENT '' AFTER `PA`,
+CHANGE `Hits` `Hits` tinyint NULL DEFAULT NULL COMMENT '' AFTER `AB`,
+CHANGE `actual_ba` `actual_ba` double NULL DEFAULT NULL COMMENT '' AFTER `Hits`,
+CHANGE `estimated_ba` `estimated_ba` double NULL DEFAULT NULL COMMENT '' AFTER `actual_ba`,
+CHANGE `estimated_minus_actual_ba` `estimated_minus_actual_ba` double NULL DEFAULT NULL COMMENT '' AFTER `estimated_ba`;
+
 update
     CombinedEvents C
-    inner join MLBIDs ID on
-    C.pitcher = ID.player
+    left join MLBIDs M on
+    C.pitcher = M.PlayerID
 set
-    C.pitcher_name = P.PlayerName
+    C.pitcher_name = M.PlayerName
 where C.pitcher_name is NULL;
-
-update CombinedEvents set actual_ba = Hits/AB;
